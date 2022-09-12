e75ae38642fed3f835b55a387e8fd5351d709755
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-12 22:47:22 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { FooterLinks } from '../components/Footer/FooterLinks';
+import { HeroBullets } from '../components/HeroBullets/HeroBullets';
 
 export default function HomePage() {
   return (
     <>
+      <HeroBullets />
       <Welcome />
       <ColorSchemeToggle />
+      <FooterLinks data={[]} />
     </>
   );
 }
