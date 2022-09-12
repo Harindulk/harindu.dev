3ff9919aff10e9ac26edd1a8ece5c1ffe75e6046
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-12 22:47:24 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { getCookie, setCookie } from 'cookies-next';
 import Head from 'next/head';
 import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import "../src/Fonts/styles.css";
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props;
@@ -22,6 +23,8 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <title>Mantine next example</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/favicon.svg" />
+
+    
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
