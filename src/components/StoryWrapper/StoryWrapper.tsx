edb1fbd4412a25ad066e1eecfb39c6feb2fce958
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-12 22:47:14 +0530

    Create StoryWrapper.tsx

---
+++ src/components/StoryWrapper/StoryWrapper.tsx
@@ -0,0 +1,15 @@
+import { ComponentPreview } from '../ComponentPreview/ComponentPreview';
+import { CanvasAttributes } from '../../data/types';
+
+interface StoryWrapperProps {
+  attributes: CanvasAttributes;
+  component: React.FC<any>;
+}
+
+export function StoryWrapper({ attributes, component: Component }: StoryWrapperProps) {
+  return (
+    <ComponentPreview canvas={attributes.canvas} withSpacing>
+      <Component {...(attributes.props || null)} />
+    </ComponentPreview>
+  );
+}
