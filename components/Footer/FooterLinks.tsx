0f787a60d7e657bbe46d06f2b6762c8d55616394
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-13 20:57:40 +0530

    Update FooterLinks.tsx

--- components/Footer/FooterLinks.tsx
+++ components/Footer/FooterLinks.tsx
@@ -140,7 +140,7 @@ export function FooterLinks({ data }: FooterLinksProps) {
       </Container>
       <Container className={classes.afterFooter}>
         <Text color="dimmed" size="sm">
-          © 2020 mantine.dev. All rights reserved.
+          © 2020 harindu.dev. All rights reserved.
         </Text>
 
         <Group spacing={0} className={classes.social} position="right" noWrap>
