640cee4e103246ad5838c2962c3ebd15ccabe26e
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-13 20:58:04 +0530

    Update Welcome.test.tsx

--- components/Welcome/Welcome.test.tsx
+++ components/Welcome/Welcome.test.tsx
@@ -4,9 +4,5 @@ import { Welcome } from './Welcome';
 describe('Welcome component', () => {
   it('has correct Next.js theming section link', () => {
     render(<Welcome />);
-    expect(screen.getByText('this guide')).toHaveAttribute(
-      'href',
-      'https://mantine.dev/guides/next/'
-    );
   });
 });
