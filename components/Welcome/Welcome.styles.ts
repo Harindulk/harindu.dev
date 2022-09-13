16ab40d44f95ccbbbf6d4448cb538b614c452c65
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-13 20:58:01 +0530

    Update Welcome.styles.ts

--- components/Welcome/Welcome.styles.ts
+++ components/Welcome/Welcome.styles.ts
@@ -3,12 +3,13 @@ import { createStyles } from '@mantine/core';
 export default createStyles((theme) => ({
   title: {
     color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    fontSize: 100,
-    fontWeight: 900,
-    letterSpacing: -2,
+    fontFamily: `Inconsolata, ${theme.fontFamily}`,
+    fontSize: 39,
+    lineHeight: 1.2,
+    fontWeight: 800,
 
-    [theme.fn.smallerThan('md')]: {
-      fontSize: 50,
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: 32,
     },
   },
 }));
