8a311bc52cf5ce9fdde78ac7aeea758cdf9e1947
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-14 22:33:52 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,3 +43,4 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
 App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
   colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
 });
+
