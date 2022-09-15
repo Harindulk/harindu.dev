91ae03ec91aa3fa6c0c83f722f5b323811090ff2
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-15 12:32:26 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,13 @@ import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { FooterLinks } from '../components/Footer/FooterLinks';
 import Demo from '../components/games/games';
+import { HeaderResponsive } from '../components/Header/Header';
+import attributes from '../components/Header/attributes.json';
 
 export default function HomePage() {
   return (
     <>
+      <HeaderResponsive links={attributes.props.links}/>
       <HomeBanner />
       <Welcome />
       <ColorSchemeToggle />
