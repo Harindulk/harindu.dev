c3b99991665209f4e8fb4d37e1569224a5810f06
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-19 23:01:59 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { HomeBanner } from '../components/HomeBanner/HomeBanner';
 import Demo from '../components/games/games';
 import { EmailBanner } from '../components/email/email';
+import Competitions from '../components/Competitions/Competitions';
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <HomeBanner />
       <Demo />
       <EmailBanner />
+      <Competitions />
     </>
   );
 }
