3761ac3b52c2967208aba0553f2ef9a85937a757
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-19 15:56:17 +0530

    cleanup

--- components/Welcome/Welcome.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Welcome } from './Welcome';
-
-describe('Welcome component', () => {
-  it('has correct Next.js theming section link', () => {
-    render(<Welcome />);
-  });
-});
