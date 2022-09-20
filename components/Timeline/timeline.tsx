13316fc9723f4c41b39b6d30ac285eb73150e57e
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-20 16:26:33 +0530

    Update timeline.tsx

--- components/Timeline/timeline.tsx
+++ components/Timeline/timeline.tsx
@@ -1,28 +1,71 @@
-import { Timeline, Text } from '@mantine/core';
-import { IconGitBranch, IconGitPullRequest, IconGitCommit, IconMessageDots } from '@tabler/icons';
+import { Timeline, Text, Space, createStyles, Container } from '@mantine/core';
 
-function Demo() {
+const useStyles = createStyles((theme) => ({
+  inner: {
+    paddingBottom: theme.spacing.xl * 3,
+  },
+
+  content: {
+    maxWidth: 510,
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%',
+      marginRight: 0,
+    },
+  },
+}));
+
+function Progress() {
+  const { classes } = useStyles();
   return (
-    <Timeline active={1} bulletSize={24} lineWidth={2}>
-      <Timeline.Item bullet={<IconGitBranch size={12} />} title="New branch">
-        <Text color="dimmed" size="sm">You&apos;ve created new branch <Text variant="link" component="span" inherit>fix-notifications</Text> from master</Text>
-        <Text size="xs" mt={4}>2 hours ago</Text>
-      </Timeline.Item>
+    <Container>
+      <div className={classes.inner}>
+        <div className={classes.content}>
+          <Timeline active={4} reverseActive lineWidth={5} bulletSize={22}>
+            <Timeline.Item title="2022">
+              <Text size="sm">
+                got first internship from 99x.
+              </Text>
+              <Space h="xs" />
+            </Timeline.Item>
 
-      <Timeline.Item bullet={<IconGitCommit size={12} />} title="Commits">
-        <Text color="dimmed" size="sm">You&apos;ve pushed 23 commits to<Text variant="link" component="span" inherit>fix-notifications branch</Text></Text>
-        <Text size="xs" mt={4}>52 minutes ago</Text>
-      </Timeline.Item>
+            <Timeline.Item title="2021">
+              <Text  size="sm">
+                created my first game called ghost zone and published it on the google play store
+                for mobile and on itch.io for windows
+              </Text>
+              <Space h="xs" />
+            </Timeline.Item>
 
-      <Timeline.Item title="Pull request" bullet={<IconGitPullRequest size={12} />} lineVariant="dashed">
-        <Text color="dimmed" size="sm">You&apos;ve submitted a pull request<Text variant="link" component="span" inherit>Fix incorrect notification message (#187)</Text></Text>
-        <Text size="xs" mt={4}>34 minutes ago</Text>
-      </Timeline.Item>
+            <Timeline.Item title="2020">
+              <Text size="sm">
+                started to learn game development 
+              </Text>
+              <Space h="xs" />
+            </Timeline.Item>
 
-      <Timeline.Item title="Code review" bullet={<IconMessageDots size={12} />}>
-        <Text color="dimmed" size="sm"><Text variant="link" component="span" inherit>Robert Gluesticker</Text> left a code review on your pull request</Text>
-        <Text size="xs" mt={4}>12 minutes ago</Text>
-      </Timeline.Item>
-    </Timeline>
+            <Timeline.Item title="2019">
+              <Text size="sm">
+               participated in many Robotics and invention competitions.
+                tried to learn HTML and CSS.
+              </Text>
+              <Space h="xs" />
+            </Timeline.Item>
+
+            <Timeline.Item title="2018">
+              <Text size="sm">
+                made my first invention and it's got selected as the most innovative idea
+              </Text>
+              <Space h="xs" />
+            </Timeline.Item>
+
+            <Timeline.Item title="2017">
+              <Text size="sm">Started doing Simple Projects using MicroBit Online Version.</Text>
+            </Timeline.Item>
+          </Timeline>
+        </div>
+      </div>
+    </Container>
   );
 }
+
+export default Progress;
