a975dc0ee3fdaab4309e44e1c678b5296a855244
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-09-20 16:26:29 +0530

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,23 +10,16 @@ import {
 } from '@mantine/core';
 import { IconCheck } from '@tabler/icons';
 import Link from 'next/link';
-import googleplay from '../public/images/playstorebutton.png';
-import appstore from '../public/images/appstorebutton.png';
-import itchio from '../public/images/itchiodark.png';
-import finalroom from '../public/images/the-final-room.jpg';
-import microraly from '../public/images/micro-rally.jpg';
-import bugstar from '../public/images/bug-star.jpg';
-import ghostzone from '../public/images/ghost-zone.jpg';
+import { SocialMedia } from '../components/SocialMedia/Socialstats';
+import attributes from '../components/SocialMedia/socialstats.json';
+import Progress from '../components/Timeline/timeline';
 
 const useStyles = createStyles((theme) => ({
   inner: {
-    display: 'flex',
-    justifyContent: 'space-around',
     paddingBottom: theme.spacing.xl * 3,
   },
 
   content: {
-    maxWidth: 580,
     justifyContent: 'center',
     display: 'flex',
     flexDirection: 'column',
@@ -86,29 +79,30 @@ export function about() {
       <Container>
         <div className={classes.inner}>
           <div className={classes.content}>
-            <Title className={classes.title}>Micro Rally</Title>
             <Text mt="md">
-              I'm Harindu Fonseka ( commonly known as harindulk ). I'm a Sri Lankan Game Developer,
+              I'm Harindu Fonseka ( commonly known as harindulk ). I'm a Sri Lankan Game Developer, <br />
               Web Developer and a Student. Verified on Google at 15 ( Sri Lanka's youngest google
-              verified age ). When I was 11, I started doing simple projects with MicroBit. I began
-              to learn Arduino when I was 12 years old and built over 30+ Arduino and NodeMcu
-              projects and participated in many Robotics and invention competitions. When I was 13,
+              verified age ). <br /><br /> When I was 11, I started doing simple projects with MicroBit. I began
+              to learn Arduino. <br /><br /> when I was 12 years old and built over 30+ Arduino and NodeMcu
+              projects and participated in many Robotics and invention competitions. <br /><br /> When I was 13,
               I tried to learn HTML and CSS and built my first website with all skills that I knew.
               I also participated in Sri Lankan Robotics Challenge 2019 and scored 12th place in Sri
-              Lanka. When I was 14, I played many games during the lockdown. One day I got an idea
+              Lanka. <br /><br /> When I was 14, I played many games during the lockdown. One day I got an idea
               to try to make my own game (I got inspiration from danidev ). After learning a lot of
               stuff from youtube (especially from brackeys and blackthornprod ), I created my first
               game called ghost zone and published it on the google play store for mobile and on
               itch.io for windows. It's a 2D arcade runner game with pretty basic controls. And
               earned a small amount of money from displaying ads on mobile devices. In 2020
-              September started my first company called "Harindulk Games." Overall I have experience
-              with C#, Unity, Java, HTML, CSS, Javascript, Unity and python. And released one game
-              on the play store and three on the itch.io store. Also completed over 30+ competitions
-              and won over 12+.
+              September started my first company called "Harindulk Games."
             </Text>
           </div>
 
         </div>
+
+        
+        <Progress />
+        {/* <SocialMedia data={attributes.data} /> */}
+
       </Container>
     </div>
   );
