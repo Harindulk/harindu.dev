1582553e2078788592483bee2875ef25ff0dcc2e
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-10-31 22:04:50 +0530

    new version 10:31:2022

---
+++ studio/schemas/experience.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'experience',
+    type: 'document',
+    title: 'experience',
+    fields: [
+        {
+        name: 'headImage',
+        title: 'Cover Image',
+        type: 'image',
+        },
+        {
+        name: 'title',
+        type: 'string',
+        title: 'Title',
+        },
+        {
+        name: 'description',
+        type: 'text',
+        title: 'Description',
+        },
+    ]
+  }
---
+++ studio/schemas/socialstats.js
@@ -0,0 +1,28 @@
+export default {
+    name: 'aboutharindu',
+    type: 'document',
+    title: 'About',
+    fields: [
+        {
+            name: 'description',
+            type: 'text',
+            title: 'Description',
+        },
+        {
+            name: 'linkedin',
+            type: 'number',
+            title: 'Linkedin',
+        },
+        {
+            name: 'twitter',
+            type: 'number',
+            title: 'Twitter',
+        },
+        {
+            name: 'github',
+            type: 'number',
+            title: 'Github',
+        }
+    ]
+}
+
