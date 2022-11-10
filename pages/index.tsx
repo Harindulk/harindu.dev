dc2080d2f95d86dcd64ba2772b7bd96a7da63094
harindulk <61319844+Harindulk@users.noreply.github.com>
2022-11-11 00:17:34 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,24 @@ import { Home } from '../components/Home';
 import { Expierience } from '../components/expierience';
 import { TableSort } from '../components/hackthons';
 import { Space,Container } from '@mantine/core';
+import { SocialProfileJsonLd } from 'next-seo';
 
 
 export default function HomePage() {
   return (
     <Container>
     <>
+    <SocialProfileJsonLd
+      type="Person"
+      name="your name"
+      url="http://www.your-site.com"
+      sameAs={[
+        'http://www.facebook.com/your-profile',
+        'http://instagram.com/yourProfile',
+        'http://www.linkedin.com/in/yourprofile',
+        'http://plus.google.com/your_profile',
+      ]}
+    />
       <Home />
       <Space h={100} />
       <Expierience />
